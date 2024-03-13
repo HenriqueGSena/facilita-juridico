6ed849f27e4d6337b4ea7d4401ea3baaf9807834
henrique.sena <carloshenrique3250@gmail.com>
2024-03-13 19:14:46 -0300

    facilit-dev-v0.01
    -  Implementando o filtro no componente tabela para buscar os dados;
    -  Removendo componente filtro;

--- frontend/src/components/AppTable.tsx
+++ frontend/src/components/AppTable.tsx
@@ -1,10 +1,13 @@
 import Table from 'react-bootstrap/Table';
+import Form from 'react-bootstrap/Form';
 import api from "../api";
 import { useEffect, useState } from 'react';
 
 
 export default function AppHeader() {
     const [data, setData] = useState<any[]>([]);
+    const [filteredData, setFilteredData] = useState<any[]>([]);
+    const [filterValue, setFilterValue] = useState<string>('');
 
     const getAllContatos = async () => {
         try {
@@ -19,10 +22,32 @@ export default function AppHeader() {
         getAllContatos();
     }, []);
 
+    useEffect(() => {
+        const filteredContacts = data.filter(contact =>
+            contact.nome.toLowerCase().includes(filterValue.toLowerCase()) ||
+            contact.email.toLowerCase().includes(filterValue.toLowerCase()) ||
+            contact.telefone.toLowerCase().includes(filterValue.toLowerCase())
+        );
+        setFilteredData(filteredContacts);
+    }, [data, filterValue]);
+
+    const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilterValue(event.target.value);
+    };
+
 
     return (
         <div>
             <div className="container text-center">
+                <Form className="container w-50 mb-4">
+                    <Form.Control
+                        type="text"
+                        placeholder="Filtro"
+                        value={filterValue} 
+                        onChange={handleFilterChange}
+                    />
+                </Form>
+
                 <Table striped bordered hover>
                     <thead>
                         <th>Nome</th>
@@ -30,7 +55,7 @@ export default function AppHeader() {
                         <th>Telefone</th>
                     </thead>
                     <tbody>
-                        {data.map(contact => (
+                        {filteredData.map(contact => (
                             <tr key={contact.id}>
                                 <td>{contact.nome}</td>
                                 <td>{contact.email}</td>
