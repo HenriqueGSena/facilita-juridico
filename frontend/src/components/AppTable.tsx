082d6ff8bd9294ce74b07338b673b21eebf58ae2
henrique.sena <carloshenrique3250@gmail.com>
2024-03-14 20:43:48 -0300

    facilit-dev-v0.01
    * Criando componente de cadastro de contatos;
    *  Inserindo botao na tela;
    *  Inserindo modal dentro do botao e criando form para cadastro de novos contatos;
    *  Atualizando pagina com novo contato;

--- frontend/src/components/AppTable.tsx
+++ frontend/src/components/AppTable.tsx
@@ -1,10 +1,11 @@
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
+import AppRegister from './AppRegister';
 import api from "../api";
 import { useEffect, useState } from 'react';
 
 
-export default function AppHeader() {
+export default function AppTable() {
     const [data, setData] = useState<any[]>([]);
     const [filteredData, setFilteredData] = useState<any[]>([]);
     const [filterValue, setFilterValue] = useState<string>('');
@@ -39,17 +40,25 @@ export default function AppHeader() {
     return (
         <div>
             <div className="container text-center">
-                <Form className="container w-50 mb-4">
-                    <Form.Control
-                        type="text"
-                        placeholder="Filtro"
-                        value={filterValue} 
-                        onChange={handleFilterChange}
-                    />
-                </Form>
+                <div className='row mb-4'>
+                    <div className='col'>
+                        <Form className="w-50 mx-auto">
+                            <Form.Control
+                                type="text"
+                                placeholder="Pesquise contato aqui"
+                                value={filterValue}
+                                onChange={handleFilterChange}
+                            />
+                        </Form>
+                    </div>
+                    <div className='col-2'>
+                        <AppRegister getAllContatos={getAllContatos} />
+                    </div>
+                </div>
 
                 <Table striped bordered hover>
                     <thead>
+                        <th />
                         <th>Nome</th>
                         <th>Email</th>
                         <th>Telefone</th>
@@ -57,6 +66,7 @@ export default function AppHeader() {
                     <tbody>
                         {filteredData.map(contact => (
                             <tr key={contact.id}>
+                                <td>{contact.id}</td>
                                 <td>{contact.nome}</td>
                                 <td>{contact.email}</td>
                                 <td>{contact.telefone}</td>
